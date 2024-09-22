b2269652c00e338650597faebbbced720dc2c568
Irteza Asad <irteza@trmedia.ca>
2024-09-22 16:29:24 +0600

    add meta description, add coming msg to work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,10 +1,15 @@
 const Work = () => {
     return (
-      <div>
-        Work page
-      </div>
+        <main className="grid grid-cols-1 gap-4 pb-4 w-full screen-max-width mt-20 mb-20">
+            <div className="grid-item col-span-1 ">
+                <div className="card text-center ">
+                    <p className="text-lg py-7 px-5">
+                        <span className="text-amber-400 font-semibold">Work in progress!</span> I'm crafting something awesome — please be patient. In the meantime, enjoy the waiting music in your head... or grab a coffee. Either way, it’ll be ready before you know it!
+                    </p>
+                </div>
+            </div>
+        </main>
     )
-  }
-  
-  export default Work
-  
+}
+
+export default Work
