b9e68065ea30fd80cebb9059215db1d9af806d4c
Irteza Asad <irteza@trmedia.ca>
2025-03-05 12:54:26 +0600

    fix: wording and link fix

--- src/components/BlogsCard.js
+++ src/components/BlogsCard.js
@@ -4,7 +4,7 @@ import { ImageCardLink, ImageMediumIcon } from '../contents'
 const BlogsCard = () => {
     return (
         <div className="group px-4 py-4">
-            <NavLink className="card-link" to="https://irtezaasadrizvi.github.io/" target="_blank" rel="noreferrer" />
+            <NavLink className="card-link" to="https://irtezaasadrizvi.medium.com/" target="_blank" rel="noreferrer" />
             <img className="mx-auto mb-6 mt-2 h-[120px]" src={ImageMediumIcon} alt='Medium' />
             <p className="card-sub-title">Blog</p>
             <p className="card-title">What I write</p>
--- src/components/ProfilesCard.js
+++ src/components/ProfilesCard.js
@@ -1,9 +1,10 @@
+import { NavLink } from "react-router-dom";
 import { ImageCardLink, ImageGithub, ImageLinkedin } from '../contents'
 
 const ProfilesCard = () => {
     return (
         <div className="group px-4 py-4">
-            {/* <NavLink className="card-link" to="/about" /> */}
+            <NavLink className="card-link" to="/about" />
             <div className="grid grid-cols-2 gap-3 p-3 mb-4 rounded-3xl border border-gray-400">
                 <a className=" rounded-full border border-gray-400 w-20 h-20 my-4 mx-auto flex items-center justify-center" href="https://github.com/IrtezaAsadRizvi" target="_blank" rel="noreferrer">
                     <img className="w-12" src={ImageGithub}  alt='Github'/>
@@ -12,8 +13,8 @@ const ProfilesCard = () => {
                     <img className="w-12" src={ImageLinkedin}  alt='Linkedin'/>
                 </a>
             </div>
-            <p className="card-sub-title">Blog</p>
-            <p className="card-title">What I write</p>
+            <p className="card-sub-title">Socials</p>
+            <p className="card-title">Find me everywhere</p>
             <img className="absolute bottom-6 right-6 opacity-30 group-hover:opacity-100" 
                     src={ImageCardLink}  alt='Link'/>
         </div>
